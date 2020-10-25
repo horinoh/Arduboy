85ef59f5082c215ebe3e4550b03cbb78f3d97e29
horinoh <development@horino.sakura.ne.jp>
2020-10-26 01:18:14 +0900

    コード更新

--- ParametricSurface/ParametricSurface.ino
+++ ParametricSurface/ParametricSurface.ino
@@ -12,13 +12,13 @@
 
 Arduboy2 arduboy;
 
-constexpr auto Width = 10;
-constexpr auto Height = 10;
-constexpr UQ8x8 InvWidth(1.0f / Width);
-constexpr UQ8x8 InvHeight(1.0f / Height);
+constexpr auto UCount = 10;
+constexpr auto VCount = 10;
+constexpr UQ8x8 InvUCount(1.0f / UCount);
+constexpr UQ8x8 InvVCount(1.0f / VCount);
 
-Vec3<SQ7x8> VertexBuffer[Height * Width];
-uint8_t IndexBuffer[(Height - 1) * (Width - 1) * 2 * 3];
+Vec3<SQ7x8> VertexBuffer[VCount * UCount];
+uint8_t IndexBuffer[(VCount - 1) * (UCount - 1) * 2 * 3];
 uint8_t PrimitiveCount = 0;
 
 template<typename T> constexpr Vec2<T> GetUV(const T& U, const T& V) { 
@@ -30,11 +30,15 @@ template<typename T> constexpr Vec2<T> GetUV(const T& U, const T& V) {
 }
 template<typename T, typename U> constexpr Vec3<T> GetPos(const Vec2<U>& InUV) {
 #ifdef DRAW_UV
-	return Vec3<T>((float)InUV.X(), (float)InUV.Y(), 0);
-#else
+	//return Vec3<T>((float)InUV.X(), (float)InUV.Y(), 0);
 	const auto UV = GetUV(InUV.X(), InUV.Y());
+	return Vec3<T>((float)UV.X(), (float)UV.Y(), 0.0f);
+#else
+	//const auto UV = GetUV(InUV.X(), InUV.Y());
+	const auto UV = Vec2<T>((float)InUV.X(), (float)InUV.Y());
 	const T InnerR = 0.5f, OuterR = 1.0f;
-	const T CU = cosFixed((float)UV.X()), CV = cosFixed((float)UV.Y()), SU = sinFixed((float)UV.X()), SV = sinFixed((float)UV.Y());
+	const T CU = cosFixed(UV.X()), CV = cosFixed(UV.Y()), SU = sinFixed(UV.X()), SV = sinFixed(UV.Y());
+	//const T CU = cosFixed((float)UV.X()), CV = cosFixed((float)UV.Y()), SU = sinFixed((float)UV.X()), SV = sinFixed((float)UV.Y());
 	return Vec3<T>((OuterR + InnerR * CV) * CU, (OuterR + InnerR * CV) * SU, InnerR * SV);
 #endif
 }
@@ -48,19 +52,19 @@ void setup() {
 
 	auto Count = 0;
 	auto V = UQ8x8(0);
-	for (uint16_t i = 0; i < Height; ++i, V += InvHeight) {
+	for (uint16_t i = 0; i < VCount; ++i, V += InvVCount) {
 		auto U = UQ8x8(0);
-		for (auto j = 0; j < Width; ++j, U += InvWidth, ++Count) {
+		for (auto j = 0; j < UCount; ++j, U += InvUCount, ++Count) {
 			VertexBuffer[Count] = GetPos<SQ7x8>(GetUV(U, V));
 		}
 	}
 
 	PrimitiveCount = 0;
 	Count = 0;
-	for (auto i = 0; i < Height - 1; ++i) {
-		const auto baseIdx = i * Width;
-		for (auto j = 0; j < Width - 1; ++j) {
-			const auto idx0 = baseIdx + j, idx1 = baseIdx + j + Width, idx2 = baseIdx + j + 1, idx3 = baseIdx + j + 1 + Width;
+	for (auto i = 0; i < VCount - 1; ++i) {
+		const auto baseIdx = i * UCount;
+		for (auto j = 0; j < UCount - 1; ++j) {
+			const auto idx0 = baseIdx + j, idx1 = baseIdx + j + UCount, idx2 = baseIdx + j + 1, idx3 = baseIdx + j + 1 + UCount;
 			IndexBuffer[Count++] = idx0;
 			IndexBuffer[Count++] = idx1;
 			IndexBuffer[Count++] = idx2;
@@ -81,16 +85,19 @@ void loop() {
 
 		const auto Start = millis(); {
 			for (auto i = 0; i < PrimitiveCount; ++i) {
-				const auto baseIdx = i * 3;
+				const auto baseIdx = i * 3;	
 				const auto idx0 = IndexBuffer[baseIdx], idx1 = IndexBuffer[baseIdx + 1], idx2 = IndexBuffer[baseIdx + 2];
 #ifdef DRAW_UV
 				arduboy.drawTriangle((float)VertexBuffer[idx0].X() * Arduboy2::width(), (float)VertexBuffer[idx0].Y() * Arduboy2::height(),
 					(float)VertexBuffer[idx1].X() * Arduboy2::width(), (float)VertexBuffer[idx1].Y() * Arduboy2::height(),
 					(float)VertexBuffer[idx2].X() * Arduboy2::width(), (float)VertexBuffer[idx2].Y() * Arduboy2::height(), WHITE);
 #else
-				arduboy.drawTriangle((float)VertexBuffer[idx0].X() * 20 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx0].Y() * 20 + (Arduboy2::height() >> 1),
-					(float)VertexBuffer[idx1].X() * 20 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx1].Y() * 20 + (Arduboy2::height() >> 1),
-					(float)VertexBuffer[idx2].X() * 20 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx2].Y() * 20 + (Arduboy2::height() >> 1), WHITE);
+				arduboy.drawTriangle((float)VertexBuffer[idx0].X() * 30 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx0].Y() * 30 + (Arduboy2::height() >> 1),
+					(float)VertexBuffer[idx1].X() * 30 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx1].Y() * 30 + (Arduboy2::height() >> 1),
+					(float)VertexBuffer[idx2].X() * 30 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx2].Y() * 30 + (Arduboy2::height() >> 1), WHITE);
+				/*arduboy.drawPixel((float)VertexBuffer[idx0].X() * 20 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx0].Y() * 20 + (Arduboy2::height() >> 1), WHITE);
+				arduboy.drawPixel((float)VertexBuffer[idx1].X() * 20 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx1].Y() * 20 + (Arduboy2::height() >> 1), WHITE);
+				arduboy.drawPixel((float)VertexBuffer[idx2].X() * 20 + (Arduboy2::width() >> 1), (float)VertexBuffer[idx2].Y() * 20 + (Arduboy2::height() >> 1), WHITE);*/
 #endif
 			}
 		} const auto End = millis();
